a2ec31ae77dc0f4fad093f24817d29c43005f8d5
manjunath3779 <manjunathraikar47@gmail.com>
2024-01-05 13:17:10 +0530

    homepage

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    borderRadius:{
+      '3xl': '64px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
